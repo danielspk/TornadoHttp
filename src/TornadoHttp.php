5bc0b989bf5082eefa4f1fbccc096f7af95a2031
Daniel M. Spiridione <dspiridione@ewwoconsulting.com>
2018-09-20 10:57:02 -0300

    Edit travis config

--- src/TornadoHttp.php
+++ src/TornadoHttp.php
@@ -19,14 +19,14 @@ use Zend\Diactoros\Response;
  * @author Daniel M. Spiridione <info@daniel-spiridione.com.ar>
  * @link http://tornadohttp.com
  * @license https://raw.githubusercontent.com/danielspk/TornadoHttp/master/LICENSE.md MIT License
- * @version 2.1.2
+ * @version 2.1.3
  */
 final class TornadoHttp implements RequestHandlerInterface
 {
     /**
      * Version
      */
-    const VERSION = '2.1.2';
+    const VERSION = '2.1.3';
 
     /**
      * @var array Global Attributes
